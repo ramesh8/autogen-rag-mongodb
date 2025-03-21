112403e4947f815874104e4b6815be1e9c1191ce
Raja Ramesh Haddubangi <h.rajaramesh@gmail.com>
2025-03-21 13:21:16 +0530

    Update sme_db_agent.py

--- sme_db_agent.py
+++ sme_db_agent.py
@@ -10,17 +10,17 @@ mongo_expert_agent = autogen.AssistantAgent(
         system_message=mongo_expert_prompt,
         llm_config=llmconfig,
         human_input_mode="NEVER",
-        is_termination_msg=lambda msg: "terminate" in msg["content"].lower(), 
+        is_termination_msg=lambda msg: '"status":"querypass"' in msg["content"].lower(), 
         
     )
 
 query_check_agent = autogen.AssistantAgent(
         name="Query_Check_Agent",
         system_message="You are a helpful AI Assistant."
-        "You have to check the given query is valid and passes the QA using the provided tool. if query passed validation just say TERMINATE.",
+        "You have to check the given query is valid and passes the QA using the provided tool.",
         llm_config=llmconfig,
         human_input_mode="NEVER",
-        is_termination_msg=lambda msg: "terminate" in msg["content"].lower(), 
+        is_termination_msg=lambda msg: '"status":"querypass"' in msg["content"].lower(), 
     )
 
 user_proxy = autogen.UserProxyAgent(
@@ -32,7 +32,7 @@ user_proxy = autogen.UserProxyAgent(
             "use_docker": False,
         },
         max_consecutive_auto_reply=10,
-        is_termination_msg=lambda msg: "terminate" in msg["content"].lower(), 
+        # is_termination_msg=lambda msg: "terminate" in msg["content"].lower(), 
     )
 
 register_function(
@@ -54,5 +54,5 @@ groupchat = autogen.GroupChat(
 manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llmconfig)
 
 group_chat_result = user_proxy.initiate_chat(
-    manager, message=f"Find 10 questions in  Java"
+    manager, message=f"Find 10 questions in Python"
 )
